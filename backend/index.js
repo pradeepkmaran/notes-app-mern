ff91e34f2dc8836619cbb4a56b01a96d64e4f9dd
Pradeep KM <pradeepkm6122004@gmail.com>
2024-09-15 19:47:26 +0530

    add update-note-pinned

--- backend/index.js
+++ backend/index.js
@@ -149,8 +149,6 @@ app.post("/add-note", authenticateToken, async (req, res) => {
       message: "Internal Server Error.",
     });
   }
-
-
 });
 
 // edit-note
@@ -200,7 +198,6 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
         message: "Internal Server Error",
       })
   }
-
 });
 
 // get-all-notes
@@ -264,6 +261,43 @@ app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
         error: true,
         message: "Internal Server Error",
       });
+  }
+});
+
+// update-note-pinned
+app.put("/update-note-pinned/:noteId", authenticateToken, async(req, res) => {
+  const noteId = req.params.noteId;
+  const { isPinned } = req.body;
+  const { user } = req.user;
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+    if(!note) {
+      return res
+        .status(404)
+        .json({
+          error: true,
+          message: "Note not found",
+        });
+    }
+
+    note.isPinned = isPinned;
+
+    await note.save();
+
+    return res
+      .json({
+        error: false,
+        note,
+        message: "Note updated successfully",
+      });
+  } catch(error) {
+    return res
+      .status(500)
+      .json({
+        error: true,
+        message: "Internal Server Error",
+      })
   }
 });
 
