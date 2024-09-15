244f4f08c0acf89d944d59a69e3ceca9212b287f
Pradeep KM <pradeepkm6122004@gmail.com>
2024-09-16 05:14:25 +0530

    add logout integrations

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -4,13 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar';
 import { useState } from 'react';
 
-const Navbar = () => {
+const Navbar = ({userInfo}) => {
 
     const [searchQuery, setSearchQuery] = useState("");
 
-    const navigate = useNavigate;
+    const navigate = useNavigate();
 
     const onLogout = () => {
+        localStorage.clear();
         navigate('/login');
     };
 
@@ -35,7 +36,7 @@ const Navbar = () => {
                 onClearSearch={onClearSearch}
             />
 
-            <ProfileInfo onLogout={onLogout}/>
+            <ProfileInfo userInfo={userInfo} onLogout={onLogout}/>
         </div>
     );
 }
